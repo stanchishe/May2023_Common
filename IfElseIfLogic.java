5b495aad7fbf514ff205b6e7b793f188fea64c1f
stanc <stanko_uzunov@yahoo.com>
2023-06-01 20:46:11 +0200

    Add conditional logic examples

---
+++ IfElseIfLogic.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class IfElseIfLogic {
+    public static void main(String[] args) {
+        // Creates an input tracker, to watch for input from the command line
+        Scanner input = new Scanner(System.in);
+        // Gets an integer from the command line and assigns it to 'a'
+        System.out.println("Enter a number!");
+        int a = input.nextInt();
+
+        // Close the scanner
+        input.close();
+
+        // Calculate the remainder
+        int remainder = a % 2;
+
+        if (a == 0) {
+            System.out.println("The number: " + a + " is zero!");
+            //Spam it some more!
+            System.out.println("The number: " + a + " is zero!");
+            System.out.println("The number: " + a + " is zero!");
+            System.out.println("The number: " + a + " is zero!");
+            System.out.println("The number: " + a + " is zero!");
+            System.out.println("The number: " + a + " is zero!");
+        } else if (remainder == 1) {
+            System.out.println("The number: " + a + " is odd!");
+        } else if (remainder == 0) {
+            System.out.println("The number: " + a + " is even!");
+        } else {
+            System.out.println("This is not possible!");
+        }
+        System.out.println("We are out!");
+    }
+}
---
+++ ShortHandIfElse.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ShortHandIfElse {
+    public static void main(String[] args) {
+        String myName = "Mark";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Try to guess the name! Enter name:");
+        String inputName = input.next();
+
+        // Close the scanner
+        input.close();
+
+        boolean nameIsCorrect = (inputName.equals(myName)) ? true : false;
+
+        if (nameIsCorrect) {
+            System.out.println("The name is correct1");
+        } else {
+            System.out.println("The name is not correct!");
+        }
+        System.out.println("We are out!");
+    }
+}
