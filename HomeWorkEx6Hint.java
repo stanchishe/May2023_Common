c2ceb1802d2891992fd13d67ff268173d637e161
stanc <stanko_uzunov@yahoo.com>
2023-06-06 21:05:36 +0200

    Add loop logic

---
+++ HomeWorkEx6Hint.java
@@ -0,0 +1,11 @@
+public class HomeWorkEx6Hint {
+    public static void main(String[] args) {
+        int initial = 12345;
+        // Start loop (if you want)
+        // Using modulo we will get  the last number
+        int remainder = initial % 10;
+
+        initial /= 10;
+        //End loop somewhere...
+    }
+}
