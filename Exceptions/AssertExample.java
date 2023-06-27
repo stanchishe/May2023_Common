bc17fc5e771e2129d0c28ab2027342b38e6d11d0
stanc <stanko_uzunov@yahoo.com>
2023-06-27 19:22:22 +0200

    Fix the assert example

--- Exceptions/AssertExample.java
+++ Exceptions/AssertExample.java
@@ -5,6 +5,7 @@ public class AssertExample {
         System.out.println("Starting the example!");
         int x = 1;
         // DIDNT WORK, WE ARE ALL SAD!
+        // STILL DOESNT WORK1
         assert (x % 2 == 0);
     }
 }
